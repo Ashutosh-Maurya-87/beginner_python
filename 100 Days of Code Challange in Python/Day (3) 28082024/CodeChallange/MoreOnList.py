0646cababb0cc21eaab29e2664da2844d4244f76
Ashutosh Maurya <ashumaurya486@gmail.com>
2024-08-28 07:59:34 +0530

    more method on list as append

---
+++ 100 Days of Code Challange in Python/Day (3) 28082024/CodeChallange/MoreOnList.py
@@ -0,0 +1,9 @@
+list = [1,3,4,5,2,2,1,4]
+
+# append method
+list.append(45)
+print(list); '''[1, 3, 4, 5, 2, 2, 1, 4, 45]'''
+
+# if we want to add in the first index then
+list[0] = 'ashu'
+print(list); '''['ashu', 3, 4, 5, 2, 2, 1, 4, 45]'''
