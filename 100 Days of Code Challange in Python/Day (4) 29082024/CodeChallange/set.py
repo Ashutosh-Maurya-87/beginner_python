80ab79584546fe6cbaa334ea2c98c0d6be6a476d
Ashutosh Maurya <ashumaurya486@gmail.com>
2024-08-29 09:03:44 +0530

    union, update, intersection, intersection_update method

--- 100 Days of Code Challange in Python/Day (4) 29082024/CodeChallange/set.py
+++ 100 Days of Code Challange in Python/Day (4) 29082024/CodeChallange/set.py
@@ -19,3 +19,48 @@ print(set1); '''{1, 2, 3, 4, 5, 6, 7, 8}, bcause set does not contain duplicate 
 a = set(); '''this creates the empty set'''
 print(a); '''set()'''
 print(type(a)); '''<class 'set'>'''
+
+# union() method
+s1 = {1,4,5,2,3,5,7,1,4,5}
+
+s2 = {4,5,6,1,2,3,4,1,4,5}
+
+print('union method:',s1.union(s2)); '''union method: {1, 2, 3, 4, 5, 6, 7}'''
+print(s1); '''{1, 2, 3, 4, 5, 7}'''
+# union method does not update the original set, it only adjoint that from specified set, without duplicate value.
+
+# update(one set is required, set2,set3...optional) method
+s1 = {'blue','green','yellow','red'}
+s2 = {'Orange','white','black','red'}
+s1.update(s2)
+print(s1); '''{'red', 'white', 'Orange', 'green', 'blue', 'black', 'yellow'}'''
+# The update() method updates the original set by adding items from all the specified sets, with no duplicates.
+# we can specify as many sets as you want, just separate each set with a comma.
+
+
+# intersection() method
+set1 = {1,2,2,3,3,4,4,5,5,8,8,9,9,4,4,3,3,2,2,33,4}
+set2 = {1,2,2,33,4,55,44,22,2,1,2,3,444,55,66,77}
+print(set1.intersection(set2)); '''{1, 2, 3, 4, 33}, contains only that item which is common in both set'''
+set1 = {1,2,2,3,3,4,4,5,5,8,8,9,9,4,4,3,3,2,2,33,4}
+set2 = {1,2,2,33,4,55,44,22,2,1,2,3,444,55,66,77}
+set3 = {44,555,6666,7777,8888,333,555,333}
+# set3 = {'ashu','aadi','dev','arush'}
+print('intersection with multiple set',set1.intersection(set2,set3)); '''intersection with multiple set set(), 
+
+it giving empty set because , not any item is matching in all the set'''
+
+set1 = {1,2,2,3,3,4,4,5,5,8,8,9,9,4,4,3,3,2,2,33,444}
+set2 = {1,2,2,33,4,55,44,22,2,1,2,3,444,55,66,77}
+set3 = {444,555,6666,7777,8888,333,555,333}
+print('intersection with multiple set',set1.intersection(set2,set3)); '''intersection with multiple set {444}'''
+A = {'red', 'green', 'blue'}
+B = {'yellow', 'orange', 'red'}
+C = {'blue', 'red', 'black'}
+print(A.intersection(B,C)); '''{'red'}'''
+
+# intersection_update() method
+s1 = {1,2,3,4,1,2,3,4,5,6,6,5}
+s2 = {1,3,4,5,6,2,3,2,5,6,3,4}
+s1.intersection_update(s2)
+print('intersection update method',s1); '''intersection update method {1, 2, 3, 4, 5, 6}, it modified the original set'''
--- 100 Days of Code Challange in Python/Day (4) 29082024/ReadMe/set.md
+++ 100 Days of Code Challange in Python/Day (4) 29082024/ReadMe/set.md
@@ -1,5 +1,5 @@
 
-what is set in python?
+# what is set in python?
   
   Set is one of 4 built-in data types in Python used to store 
   collections of data, the other 3 are List, Tuple, and Dictionary, 
@@ -15,3 +15,19 @@ Set is collection of item and enclosed with curly braces {}.
    A set is a collection which is unordered, *unchangable, and unindexed.
 
    * Note: Set items are unchangeable, but you can remove items and add new items.
+
+# union(set name required) method
+  union() method return a set that contain all items from the original set, and all item
+  from the specified set which is given inside the union method.
+  If an item is present in more than one set, the result will contain only one appearance of this item.
+
+  ############################
+* The union() methods joins all items from both sets, means it contains only that unique item which is present in both set and 
+update() methods joins all items from both sets, means it update the original set and join that. 
+The intersection() method keeps ONLY the duplicates, means it contains only that item which is present in both set. 
+The difference() method keeps the items from the first set that are not in the other set(s). 
+The symmetric_difference() method keeps all items EXCEPT the duplicates.
+
+# intersection_update(set1,set2...) method
+ * set1 is required, set2 is optional
+ The intersection_update() method updates the set by removing the items that are not common to all the specified sets.
