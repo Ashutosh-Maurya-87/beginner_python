336e1de8a505a91e82118bf97627669e67b53819
Ashutosh Maurya <ashumaurya486@gmail.com>
2024-08-30 08:50:00 +0530

    tuple data type in python SOme more method in tuple

--- 100 Days of Code Challange in Python/Day (5) 30082024/CodeChallange/tuple.py
+++ 100 Days of Code Challange in Python/Day (5) 30082024/CodeChallange/tuple.py
@@ -35,3 +35,31 @@ print(t[1:3]); '''(2, 3.4)'''
 print(t[1:8:2]); '''(2, 5, 7, 3), third value is jumping , means we want to skip that number of value'''
 print(t[:]); '''(1, 2, 3.4, 5, 6, 7, 4, 3, 2, 'ashu', 45, 6, 7, 3)'''
 print(t[0:5]); '''(1, 2, 3.4, 5, 6)'''
+
+
+# we can perform add, delete and update in the tuple, but first we neet to convert that tuple in the list
+ # and after performing that method from list to tuple
+ # we will do this because tuple is not changable, means they are immutable we can't alter them
+
+ # converting tuple to list is :
+t = (1,2,3,4,4,6,7)
+# t[1] = '4'
+print(t); '''TypeError: 'tuple' object does not support item assignment'''
+x = list(t)
+print(type(x)); '''<class 'list'>'''
+x[0] = 'ashu'
+print(x); '''['ashu', 2, 3, 4, 4, 6, 7]'''
+x.append(45)
+print(x); '''['ashu', 2, 3, 4, 4, 6, 7, 45]'''
+
+# now after perfoming the method , we converting list to tuple
+a = tuple(x)
+print(a, type(a)); '''('ashu', 2, 3, 4, 4, 6, 7, 45) <class 'tuple'>'''
+
+# index(value,startIndex, endIndex) method
+print(t.index(4)); '''3, this is the index number'''
+
+t = (1,2,3,4,3,5,2,3,5,4,5,6,4,3,2,3,3,4)
+print(t.index(3,2,4)); '''2, this is the index number'''
+
+print(t.count(3)); '''6, time 3 is occure in the tuple'''
--- 100 Days of Code Challange in Python/Day (5) 30082024/ReadMe/tuple.md
+++ 100 Days of Code Challange in Python/Day (5) 30082024/ReadMe/tuple.md
@@ -7,3 +7,12 @@
 * if we write tuple = (2) then python convert this is 'int' type then python did not find that is tuple.
   so overcome this problem we write an comma after the value
     tuple = (2,) now python will find that is tuple type.
+
+* if we want to alter the tuple then we convert that tuple to the list and after performing operations
+  we convert again list to tuple
+
+# index(value, startIndex, endIndex)
+* The index() method searches for the first occurrence of the given item and returns its index. 
+  If specified item is not found, it raises ‘ValueError’ exception.
+  * startIndex -> An index specifying where to start the search.Default is 0.
+  * endIndex -> An index specifying where to stop the search.Default is the end of the tuple.
