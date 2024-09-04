3cee327ff60f24bb6a7c841a48b1f3c72f015e1d
Ashutosh Maurya <ashumaurya486@gmail.com>
2024-09-04 08:39:04 +0530

    function in  python

---
+++ 100 Days of Code Challange in Python/Day (9) 04092024/function.md
@@ -0,0 +1,6 @@
+# Function Arguments
+* There are four type of function Arguments
+  * Default Arguments
+  * Keyword Arguments
+  * Variable Length Arguments
+  * Required Lenth Arguments
---
+++ 100 Days of Code Challange in Python/Day (9) 04092024/function.py
@@ -0,0 +1,50 @@
+
+# predefined function
+# takeInput = input('enter any thing')
+
+# In python we defined function using def keyword
+
+# definition of function, normal function, without parameter
+def name() :
+    print('we defined our first function')
+
+# calling function
+name()
+
+# function with parameter
+
+def greet(msg) :
+    print(f'hello Good Morning! {msg}')
+
+greet('Ashutosh'); '''ello Good Morning! Ashutosh'''
+
+# Default arguments in the function
+def add(a=5,b=6) :
+    print('function with default arguments', a + b)
+
+add(); '''function with default arguments 11'''
+add(10,20); '''after adding it give 30, because it ignore the default vaue and take the passed value whatever we pass
+during calling the function, if we don't pass any value then it take default value and print that''' 
+'''
+function with default arguments 11
+function with default arguments 30
+'''
+
+# Required Arguments in Funtion
+def avg(a,b,c) :
+    print('average is:',(a+b+c)/3)
+
+avg(10,10,10); '''average is: 10.0'''
+l = [1,2,3,4,5]
+greet(l); '''hello Good Morning! [1, 2, 3, 4, 5]'''
+
+# Variable lenth arguments
+'''if we add * and then parameter name, then function take value as a tuple, and we can pass multiple argument,
+while calling the function'''
+def AvgOfMoreNUmber(*num) :
+    sum = 0
+    for x in num:
+        sum = sum + x
+    print('average of numbers is:', sum/len(num))
+
+AvgOfMoreNUmber(1,2,3,4,5,6,7,8,9,10); '''average of numbers is: 5.5'''
