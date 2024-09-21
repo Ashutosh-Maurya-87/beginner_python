7541ea9826db411ceada58b056b8a06a9d89dcca
Ashutosh Maurya <ashumaurya486@gmail.com>
2024-09-21 16:04:49 +0530

    class variable and instance variables

--- 100 Days of Code Challange in Python/Day (12) 20092024/classObject.py
+++ 100 Days of Code Challange in Python/Day (12) 20092024/classObject.py
@@ -30,3 +30,65 @@ t.englishFun(); '''tody you will learn A girl with the basket with tacher name K
 t1 = tenthClass()
 t1.time = "25 mins"
 t1.englishFun(); '''tody you will learn A girl with the basket with tacher name Khargosh in the time of 25 mins'''
+
+
+class studentDetails:
+    '''these are class variables'''
+    name = 'Aadi'
+    gender = 'male'
+    hobbies = 'Cricket and Riding'
+    def __init__(self,name,gender,hobbies,age,address):
+        '''these are instance varibales'''
+        self.name = name
+        self.gender = gender
+        self.hobbies = hobbies
+        self.age = age
+        self.address = address
+
+
+s = studentDetails('deva','male','skiting',22,'haridwar')
+'''access of instance variable'''
+print('stuent details', s.name)
+print('stuent details', s.gender)
+print('stuent details', s.hobbies)
+'''
+stuent details deva
+stuent details male
+stuent details skiting
+'''
+
+print('stuent details', s.age)
+print('stuent details', s.address)
+'''
+stuent details 22
+stuent details haridwar
+'''
+
+# Accessing the class variables
+print('class variable student details', studentDetails.name)
+print('class variable student details', studentDetails.gender)
+print('class variable student details', studentDetails.hobbies)
+'''
+class variable student details Aadi
+class variable student details male
+class variable student details Cricket and Riding
+'''
+
+class test:
+    num = 5
+    def add(self,a,b):
+        self.a=a
+        self.b=b
+        sum = self.a+self.b
+        return sum
+    
+t = test()
+print('t value',t.add(2,5)); '''t value 7'''
+print('valkue of a is', t.a)
+print('valkue of a is', t.b)
+'''
+valkue of a is 2
+valkue of a is 5
+'''
+# by using class name we can access the class variables
+print('class variables value is', test.num); '''class variables value is 5'''
